bc8d3962c96e286403fee511e4bd6899270ed4f5
Mariev1 <marie.s.hagen@gmail.com>
2022-05-15 17:01:45 +0200

    meh

---
+++ js/question-1.js
@@ -0,0 +1,14 @@
+//function getRemainder(a,b) {
+//    return a % b;
+//    }
+
+const getRemainder = (a,b) => a % b;
+        
+const result = getRemainder(5, 3);        
+
+console.log(result);     
+
+
+
+    
+
---
+++ js/question-2.js
@@ -0,0 +1,26 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b762d9f71d2042c693e43c7cea99413c";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getGames() {
+
+    const response = await fetch(url);
+    const results = await response.json();
+    const games = results.results;
+    const tags = results.games_count;
+
+    resultsContainer.innerHTML = "";
+
+    for (let i = 0; i < games.length; i++) {
+        console.log(games[i].games_count);
+
+        if (i === 8) {
+            break;
+        }
+
+        resultsContainer.innerHTML += `<div><h2>${games[i].name}</h2><p>${games[i].rating}</p><p>${games[i].tags}</p></div>`;
+    }
+
+}
+
+getGames();
